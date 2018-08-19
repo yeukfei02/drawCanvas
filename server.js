a01de305bb8730edfb99c7c569190e14bab0a05a
Donald Wu <yeukfei02@gmail.com>
2018-08-19 23:56:06 +0800

    update for heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
@@ -10,11 +8,10 @@ app.set('port', (process.env.PORT || 3000));
 app.use('/bower_components', express.static(path.join(__dirname, '/app/bower_components')));
 app.use('/public', express.static(path.join(__dirname, '/app/public')));
 
-http.listen(app.get('port'), function () {
-    console.log('Server is listening at port: ' + app.get('port'));
+http.listen(app.get('port'), () => {
+    console.log('server is listening at port: ' + app.get('port'));
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/app/index.html'));
-
 });
